df1fd3c82ed170d18f2c35113855a547b225d22a
AlejoCasti <acastiblanco8@gmail.com>
2020-09-09 16:43:40 +0000

    [FIX] pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,14 +8,18 @@ from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-API_HOST = environ.get('HBNB_API_HOST') if environ.get('HBNB_API_HOST') else '0.0.0.0'
-API_PORT = environ.get('HBNB_API_PORT') if environ.get('HBNB_API_PORT') else '5000'
- 
+API_HOST = environ.get('HBNB_API_HOST')\
+           if environ.get('HBNB_API_HOST') else '0.0.0.0'
+API_PORT = environ.get('HBNB_API_PORT')\
+           if environ.get('HBNB_API_PORT') else '5000'
+
 
 @app.teardown_appcontext
 def close(error):
     ''' close storage '''
     storage.close()
 
+
 if __name__ == '__main__':
+    ''' main funciton to run flask '''
     app.run(host=API_HOST, port=API_PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 ''' routes and views of web page '''
 from api.v1.views import app_views
 from flask import jsonify
+
 
 @app_views.route('/status')
 def route_status():
